68085e29c4c4e028761b378fb54097de2161d563
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-11-25 22:05:49 +0300

    Done assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :only_one
+    def index
+        users = User.all
+        render json: users, include: :items
+    end
+
+    def show
+        user = User.find(params[:id])
+         render json: user, include: :items
+        
+    end
+
+    private
+        def only_one
+            render json: { error: "not found" }, status: :not_found
+        end
 end
